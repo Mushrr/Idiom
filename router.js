b12bd8545cfd7857d6ec1ce23f0386f8c86c1aba
Mushrr <mushroom@mails.ccnu.edu.cn>
2022-06-24 09:40:14 +0800

    swagger support

--- router.js
+++ router.js
@@ -2,12 +2,10 @@
 const Router = require('koa-router');
 
 const {indexRoute} = require('./routes/indexRoute');
-const {swaggerRoute} = require('./routes/swaggerRoute');
 
 const router = new Router();
 
 router.use('/', indexRoute.routes(), indexRoute.allowedMethods());
-router.use('/doc', swaggerRoute.routes(), swaggerRoute.allowedMethods());
 
 module.exports = {
     router
